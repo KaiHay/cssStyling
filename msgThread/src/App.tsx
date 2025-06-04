062a3b3ab6c068bebac487b6bd3873f1075289c0
Kai Hay <kai@Kais-MacBook-Air.local>
2025-06-03 20:02:52 -0400

    reformatted to align properly

--- msgThread/src/App.tsx
+++ msgThread/src/App.tsx
@@ -26,15 +26,29 @@ const ListPost = () => {
         <div className='font-[Inter] font-light text-3xl p-4'>Post</div>
         <div className='text-gray-400 pl-15'>Post</div>
         <div className='flex flex-row p-4'>
-          <img className='object-scale-down h-8' src={profilePic_url} />
-          <div className='flex flex-col pl-3 text-notmeGray font-light'>
-            <div className='text-notmeGray font-light'><span className='text-black font-normal'>{exPost.Name}</span> in {exPost.GroupName}</div>
-            <div>{exPost.TimeSincePost} min ago</div>
+          <div className='flex flex-row'>
+            <div>
+              <img className='object-scale-down h-8' src={profilePic_url} />
+            </div>
+            <div>
+              <div className='flex flex-col pl-3 text-notmeGray font-light'>
+                <div className='text-notmeGray font-light'><span className='text-black font-normal'>{exPost.Name}</span> in {exPost.GroupName}</div>
+                <div>{exPost.TimeSincePost} min ago</div>
+              </div>
+
+              <div className='pl-3 flex justify-center'><img src={image_url} /></div>
+              <div className='pl-3 pt-3 font-light'>{exPost.description}</div>
+              <div className='flex flex-row items-center text-sm pt-2'>
+                <img className='h-3 pl-3 justify-center' src='../public/profilepics/heart.png' />
+                <div className='pl-2 '>{exPost.likes} likes</div>
+                <img className='h-3 pl-3 justify-center' src='../public/profilepics/chatIcon.png' />
+                <div className='pl-2 '>{exPost.commentNumber} Comments</div>
+
+              </div>
+            </div>
           </div>
         </div>
-        <div className='flex justify-center'><img src={image_url} /></div>
-        <div className=''>{exPost.description}</div>
-      </div></div>
+      </div></div >
 
   )
 }
